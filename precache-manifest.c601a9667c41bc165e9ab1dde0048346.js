011ffd57b170827f939e56540e78974941469b88
jobando <jobando@pcaingenieria.com>
2022-01-05 11:49:47 -0500

    deploy patrones render

---
+++ precache-manifest.c601a9667c41bc165e9ab1dde0048346.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f3b933b5deb9c3db69",
+    "url": "/todo/static/css/main.d351834b.chunk.css"
+  },
+  {
+    "revision": "71f3b933b5deb9c3db69",
+    "url": "/todo/static/js/main.71f3b933.chunk.js"
+  },
+  {
+    "revision": "8e21b5741c56e0588acf",
+    "url": "/todo/static/js/1.8e21b574.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "6ffe378547436e0f660bb19c09561047",
+    "url": "/todo/index.html"
+  }
+];
